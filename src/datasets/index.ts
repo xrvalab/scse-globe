7d1c72a71aa4525c8c02ba00b7bb39aec5e4d7b7
pbutcher <pbutcher93@gmail.com>
2024-11-20 00:56:29 +0000

    Add datasets

---
+++ src/datasets/index.ts
@@ -0,0 +1,6 @@
+export { default as alumniPath } from "./alumni.json";
+export { default as organisationsPath } from "./organisations.json";
+export { default as markersPath } from "./markers.json";
+
+// Geojson
+export { default as hexMapPath } from "./geojson/ne_110m_admin_0_countries.json";
